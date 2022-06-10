d79e1893d73ace89201483d96aa099fdb5c6e24b
yannnbingz <yanbing.zhou.zyb@gmail.com>
2022-06-10 10:38:39 -0400

    test

--- workflow_defs.py
+++ workflow_defs.py
@@ -76,31 +76,31 @@ def compute_pyscf_energy(mol, method="fci", **kwargs):
     return energy
 
 
-# @sdk.task()
-# def geometry_def(geo_name):
+@sdk.task()
+def geometry_def(geo_name):
 
-#     H2 = {"schema": "molecular_geometry",
-#             "sites": [
-#                         {"species": "H","x": 0,"y": 0,"z": 0},
-#                         {"species": "H","x": 0,"y": 0,"z": 0.7},
-#                         ]
-#             }
-#     H4 = {"schema": "molecular_geometry",
-#                 "sites": [
-#                             {"species": "H","x": 0,"y": 0,"z": 0},
-#                             {"species": "H","x": 0,"y": 0,"z": 0.75},
-#                             {"species": "H","x": 0.75,"y": 0,"z": 0.0},
-#                             {"species": "H","x": 0.75,"y": 0,"z": 0.75},
-#                          ]
-#           }
-#     Li = {"schema": "molecular_geometry",
-#             "sites": [
-#                         {"species": "Li","x": 0,"y": 0,"z": 0},
-#                      ]
-#                 }
+    H2 = {"schema": "molecular_geometry",
+            "sites": [
+                        {"species": "H","x": 0,"y": 0,"z": 0},
+                        {"species": "H","x": 0,"y": 0,"z": 0.7},
+                        ]
+            }
+    H4 = {"schema": "molecular_geometry",
+                "sites": [
+                            {"species": "H","x": 0,"y": 0,"z": 0},
+                            {"species": "H","x": 0,"y": 0,"z": 0.75},
+                            {"species": "H","x": 0.75,"y": 0,"z": 0.0},
+                            {"species": "H","x": 0.75,"y": 0,"z": 0.75},
+                         ]
+          }
+    Li = {"schema": "molecular_geometry",
+            "sites": [
+                        {"species": "Li","x": 0,"y": 0,"z": 0},
+                     ]
+                }
 
-#     geo_dict = {"h2": H2, "h4": H4, "li": Li}
-#     return geo_dict[geo_name]
+    geo_dict = {"h2": H2, "h4": H4, "li": Li}
+    return geo_dict[geo_name]
 
 
 @sdk.workflow
@@ -108,13 +108,7 @@ def benchmarking_h2():
     """Workflow that generates random samples and fits them using a linear
     regression."""
     # parameter input
-    #geometry = geometry_def('h2')
-    geometry = {"schema": "molecular_geometry",
-            "sites": [
-                        {"species": "H","x": 0,"y": 0,"z": 0},
-                        {"species": "H","x": 0,"y": 0,"z": 0.7},
-                        ]
-            }
+    geometry = geometry_def('h2')
     n_pno = 2
     pyscf_method = 'ccsd(t)'
 
