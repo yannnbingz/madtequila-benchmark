9f6189b5d32dc74daf7b9b750c7b7c95521bbfd8
yannnbingz <yanbing.zhou.zyb@gmail.com>
2022-06-10 10:45:49 -0400

    test

--- workflow_defs.py
+++ workflow_defs.py
@@ -25,7 +25,7 @@ def geometry_def(geo_name):
                         {"species": "H","x": 0,"y": 0,"z": 0},
                         {"species": "H","x": 0,"y": 0,"z": 0.7},
                         ]
-            }
+        }
     H4 = {"schema": "molecular_geometry",
                 "sites": [
                             {"species": "H","x": 0,"y": 0,"z": 0},
@@ -33,14 +33,16 @@ def geometry_def(geo_name):
                             {"species": "H","x": 0.75,"y": 0,"z": 0.0},
                             {"species": "H","x": 0.75,"y": 0,"z": 0.75},
                          ]
-          }
-    Li = {"schema": "molecular_geometry",
-            "sites": [
-                        {"species": "Li","x": 0,"y": 0,"z": 0},
-                     ]
-                }
+        }
+    # lithium = {"schema": "molecular_geometry",
+    #             "sites": [
+    #                         {"species": "Li","x": 0,"y": 0,"z": 0}
+    #                     ]
+    #     }
 
-    geo_dict = {"h2": H2, "h4": H4, "li": Li}
+    geo_dict = {"h2": H2, "h4": H4, 
+    # "li": lithium
+    }
     return geo_dict[geo_name]
 
 @sdk.task(
