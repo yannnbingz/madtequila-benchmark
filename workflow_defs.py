386d69aba11852e10722f58ea1f882323afc20d0
yannnbingz <yanbing.zhou.zyb@gmail.com>
2022-06-27 12:49:26 -0400

    update

--- workflow_defs.py
+++ workflow_defs.py
@@ -19,51 +19,8 @@ PYSCF_IMPORT = sdk.GitImport(
 
 @sdk.task(
     source_import=THIS_IMPORT,
-    dependency_imports=[TEQUILA_IMPORT],
-    custom_image="jgonthier/madtequila:latest",
-    n_outputs=4,
-    #resources=sdk.Resources(cpu='6000m', memory='6Gb')
-)
-def run_madness(name, geometry, n_pno, frozen_core=True, maxrank=None, **kwargs):
-    import tequila as tq
-    
-    mol = tq.Molecule(
-                        name = name,
-                        geometry = geometry,
-                        frozen_core = frozen_core,
-                        n_pno = n_pno,
-                        maxrank = maxrank,
-                        **kwargs
-                        )
-    print("*** MOL OBJECT DEFINED, INTEGRALS GENERATED ***")
-
-    with open (name + '_pnoinfo.txt', 'r') as f:
-        for line in f.readlines():
-            if "nuclear_repulsion" in line:
-                nuclear_repulsion = float(line.split("=")[1])
-            elif "pairinfo" in line:
-                pairinfo = line.split("=")[1].split(",")
-                #pairinfo = [tuple([int(i) for i in x.split(".")]) for x in pairinfo]
-            elif "occinfo" in line:
-                occinfo = line.split("=")[1].split(",")
-                occinfo = [float(x) for x in occinfo]
-
-    h, g = mol.read_tensors(name=name)
-
-
-    results_dict = {}
-    results_dict['nuclear_repulsion'] = nuclear_repulsion
-    results_dict['pairinfo'] = pairinfo
-    results_dict['occinfo'] = occinfo
-    results_dict['n_pno'] = n_pno
-
-    return mol, results_dict, h, g
-
-
-@sdk.task(
-    source_import=THIS_IMPORT,
-    dependency_imports=[PYSCF_IMPORT],
-    custom_image="jgonthier/madtequila:latest",
+    #dependency_imports=[PYSCF_IMPORT],
+    custom_image="yannnbingz/m1-madness-tequila:amd64",
     n_outputs=1
 )
 def compute_pyscf_energy():
@@ -84,23 +41,6 @@ def compute_pyscf_energy():
 def benchmarking_project():
     """Workflow that generates random samples and fits them using a linear
     regression."""
-    # parameter input
-    mol_name = 'he'
-    n_pno = 2
-    maxrank = 2
-    pyscf_method = 'hf'
-    frozen_core=False
-    geometry = 'he 0.0 0.0 0.0'
-
-    # compute mra-pno 1 and 2 body integrals from madness
-    mol, madmolecule, h, g = run_madness( name=mol_name, 
-                                    geometry=geometry, 
-                                    n_pno=n_pno, 
-                                    frozen_core=frozen_core, 
-                                    maxrank=maxrank
-                                    )
-
-    # compute energy from pyscf
     result = compute_pyscf_energy()
 
     return (madmolecule, result)
