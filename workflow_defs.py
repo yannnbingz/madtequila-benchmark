dab510ba8046c81472389b29f293e9b7b0495316
yannnbingz <yanbing.zhou.zyb@gmail.com>
2022-07-25 16:35:35 -0400

    update

--- workflow_defs.py
+++ workflow_defs.py
@@ -7,7 +7,7 @@ THIS_IMPORT = sdk.GitImport(
 )
 
 # TEQUILA_IMPORT = sdk.GitImport(
-#     repo_url="git@github.com:tequilahub/tequila.git",
+#     repo_url="git@gio-thub.com:tequilahub/tequila.git",
 #     git_ref="devel",
 # )
 
@@ -17,7 +17,7 @@ CUSTOM_IMAGE = "jgonthier/madtequila:latest"
     source_import=THIS_IMPORT,
     # dependency_imports=[TEQUILA_IMPORT],
     custom_image=CUSTOM_IMAGE,
-    resources=sdk.Resources(cpu='4000m',memory='30Gi', disk='20Gi')
+    resources=sdk.Resources(cpu='8000m',memory='60Gi', disk='20Gi')
 )
 def run_madness(name, geometry, n_pno, frozen_core=True, maxrank=None, **kwargs):
     import tequila as tq
@@ -60,7 +60,7 @@ def benchmarking_project():
 
     # parameter input: simple He test
     mol_name = 'h2o'
-    n_pno = 8
+    n_pno = 40
     maxrank = 2
     frozen_core=True
     geometry = 'h 0.0 0.7547 -0.521394777902 \n h 0.0 -0.7547 -0.521394777902 \n o 0.0 0.0 0.065705222098'
